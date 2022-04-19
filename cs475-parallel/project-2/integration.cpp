09c32eb36757bf21f8fad97ca5e0df933a319005
Robert Detjens <github@detjens.dev>
2022-04-19 13:50:51 -0700

    initial project 2 work
    Signed-off-by: Robert Detjens <github@detjens.dev>

---
+++ cs475-parallel/project-2/integration.cpp
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <omp.h>
+#include <stdlib.h>
+#include <time.h>
+
+// print debugging messages?
+#ifndef DEBUG
+#define DEBUG false
+#endif
+
+// default number of threads
+#ifndef NUMT
+#define NUMT 2
+#endif
+
+// default number of nodes
+#ifndef NUMNODES
+#define NUMNODES 1
+#endif
+
+#define XMIN -1.
+#define XMAX 1.
+#define YMIN -1.
+#define YMAX 1.
+
+float Height(int, int); // function prototype
+
+int main(int argc, char *argv[]) {
+
+  // set the number of threads to use in parallelizing the for-loop:`
+  omp_set_num_threads(NUMT);
+
+  // the area of a single full-sized tile:
+  // (not all tiles are full-sized, though)
+
+  float tile_area = ((XMAX - XMIN) / (float)(NUMNODES - 1)) *
+                    ((YMAX - YMIN) / (float)(NUMNODES - 1));
+
+  double time_start = omp_get_wtime();
+
+  // sum up the weighted heights into the variable "volume"
+  // using an OpenMP for-loop and a reduction:
+
+  double total_volume = 0;
+#pragma omp parallel for collapse(2) default(none) firstprivate(tile_area) reduction(+ : total_volume)
+  // The (2) means you are collapsing 2 nested for-loops into one
+  // The end effect is exactly like above, but without the mod / divide
+  for (int iv = 0; iv < NUMNODES; iv++) {
+    for (int iu = 0; iu < NUMNODES; iu++) {
+
+      float tile_volume = Height(iu, iv) * tile_area;
+
+      // adjust volume for edges / corners where half/quarter are cut off
+      if (iv == 0 || iv == NUMNODES - 1) {
+        tile_volume *= 0.5;
+      }
+      if (iu == 0 || iu == NUMNODES - 1) {
+        tile_volume *= 0.5;
+      }
+
+      total_volume += tile_volume;
+    }
+  }
+
+  double time_end = omp_get_wtime();
+  double megaTrialsPerSecond =
+      (double)NUMNODES / (time_end - time_start) / 1000000.;
+
+#ifndef QUIET
+  fprintf(stderr, "%2d threads : %8d nodes ; megatrials/sec = %6.2lf\n", NUMT,
+          NUMNODES, megaTrialsPerSecond);
+#endif
+#ifdef CSV
+  printf("%2d,%8d,%6.2lf\n", NUMT, NUMNODES, megaTrialsPerSecond);
+#endif
+}
+
+// The code to evaluate the height at a given iu and iv is:
+
+const float N = 2.5f;
+const float R = 1.2f;
+
+float Height(int iu, int iv) { // iu,iv = 0 .. NUMNODES-1
+
+  float x = -1. + 2. * (float)iu / (float)(NUMNODES - 1); // -1. to +1.
+  float y = -1. + 2. * (float)iv / (float)(NUMNODES - 1); // -1. to +1.
+
+  float xn = pow(fabs(x), (double)N);
+  float yn = pow(fabs(y), (double)N);
+  float rn = pow(fabs(R), (double)N);
+  float r = rn - xn - yn;
+  if (r <= 0.)
+    return 0.;
+  float height = pow(r, 1. / (double)N);
+  return height;
+}
---
+++ cs475-parallel/project-2/report.md
@@ -0,0 +1,27 @@
+---
+header-includes:
+  - \usepackage{pgfplots}
+
+title: 'CS 475 Project 2: Numeric Integration'
+author:
+  - Robert Detjens
+  - detjensr@oregonstate.edu
+---
+
+## Graphs
+
+- Tell what machine you ran this on
+
+- What do you think the actual volume is?
+
+- Show the performances you achieved in tables and two graphs showing:
+  - Performance as a function of NUMNODES with colored lines showing different NUMT values
+  - Performance as a function of NUMT with colored lines showing different NUMNODES values
+
+- What patterns are you seeing in the speeds?
+
+- Why do you think it is behaving this way?
+
+- What is the Parallel Fraction for this application, using the Inverse Amdahl equation?
+
+- Given that Parallel Fraction, what is the maximum speed-up you could ever get?
