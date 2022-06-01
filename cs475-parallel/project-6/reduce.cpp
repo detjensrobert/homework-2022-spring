9a6cf9213380a4d9fa5b3743e5e095d674d1db9e
detjensrobert <github@detjens.dev>
2022-05-31 22:39:20 -0700

    project 6 done
    Signed-off-by: detjensrobert <github@detjens.dev>

---
+++ cs475-parallel/project-6/reduce.cpp
@@ -0,0 +1,258 @@
+// 1. Program header
+
+// set desired opencl version before including headers
+#define CL_TARGET_OPENCL_VERSION 110
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <omp.h>
+#include <unistd.h>
+
+#include "CL/cl.h"
+#include "CL/cl_platform.h"
+
+#ifndef NUM_ELEMENTS
+#define NUM_ELEMENTS (64 * 1024 * 1024)
+#endif
+
+#ifndef LOCAL_SIZE
+#define LOCAL_SIZE 64
+#endif
+
+#define NUM_WORK_GROUPS NUM_ELEMENTS / LOCAL_SIZE
+
+#ifndef CL_FILE_NAME
+#define CL_FILE_NAME "reduce.cl"
+#endif
+
+void Wait(cl_command_queue);
+int LookAtTheBits(float);
+
+int main(int argc, char *argv[]) {
+  // see if we can even open the opencl kernel program
+  // (no point going on if we can't):
+
+  FILE *fp = fopen(CL_FILE_NAME, "r");
+  if (fp == NULL) {
+    fprintf(stderr, "Cannot open OpenCL source file '%s'\n", CL_FILE_NAME);
+    return 1;
+  }
+
+  cl_int status; // returned status from opencl calls
+                 // test against CL_SUCCESS
+
+  // get the platform id:
+
+  cl_platform_id platform;
+  status = clGetPlatformIDs(1, &platform, NULL);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "clGetPlatformIDs failed (2)\n");
+
+  // get the device id:
+
+  cl_device_id device;
+  status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "clGetDeviceIDs failed (2)\n");
+
+  // 2. allocate the host memory buffers:
+
+  float *host_A = new float[NUM_ELEMENTS];
+  float *host_B = new float[NUM_ELEMENTS];
+  float *host_sums = new float[NUM_WORK_GROUPS];
+
+  // fill the host memory buffers:
+
+  for (int i = 0; i < NUM_ELEMENTS; i++) {
+    host_A[i] = host_B[i] = (float)sqrt((double)i);
+  }
+
+  size_t dataSize = NUM_ELEMENTS * sizeof(float);
+  size_t sumsSize = NUM_WORK_GROUPS * sizeof(float);
+
+  // 3. create an opencl context:
+
+  cl_context context = clCreateContext(NULL, 1, &device, NULL, NULL, &status);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "clCreateContext failed\n");
+
+  // 4. create an opencl command queue:
+
+  cl_command_queue cmdQueue = clCreateCommandQueue(context, device, 0, &status);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "clCreateCommandQueue failed\n");
+
+  // 5. allocate the device memory buffers:
+
+  cl_mem device_A =
+      clCreateBuffer(context, CL_MEM_READ_ONLY, dataSize, NULL, &status);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "clCreateBuffer failed (1)\n");
+
+  cl_mem device_B =
+      clCreateBuffer(context, CL_MEM_READ_ONLY, dataSize, NULL, &status);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "clCreateBuffer failed (2)\n");
+
+  cl_mem device_sums =
+      clCreateBuffer(context, CL_MEM_WRITE_ONLY, sumsSize, NULL, &status);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "clCreateBuffer failed (4)\n");
+
+  // 6. enqueue the 2 commands to write the data from the host buffers to the
+  // device buffers:
+
+  status = clEnqueueWriteBuffer(cmdQueue, device_A, CL_FALSE, 0, dataSize,
+                                host_A, 0, NULL, NULL);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "clEnqueueWriteBuffer failed (1)\n");
+
+  status = clEnqueueWriteBuffer(cmdQueue, device_B, CL_FALSE, 0, dataSize,
+                                host_B, 0, NULL, NULL);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "clEnqueueWriteBuffer failed (2)\n");
+
+  Wait(cmdQueue);
+
+  // 7. read the kernel code from a file:
+
+  fseek(fp, 0, SEEK_END);
+  size_t fileSize = ftell(fp);
+  fseek(fp, 0, SEEK_SET);
+  char *clProgramText = new char[fileSize + 1]; // leave room for '\0'
+  size_t n = fread(clProgramText, 1, fileSize, fp);
+  clProgramText[fileSize] = '\0';
+  fclose(fp);
+  if (n != fileSize)
+    fprintf(stderr,
+            "Expected to read %zu bytes read from '%s' -- actually read %zu.\n",
+            fileSize, CL_FILE_NAME, n);
+
+  // create the text for the kernel program:
+
+  char *strings[1];
+  strings[0] = clProgramText;
+  cl_program program = clCreateProgramWithSource(
+      context, 1, (const char **)strings, NULL, &status);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "clCreateProgramWithSource failed\n");
+  delete[] clProgramText;
+
+  // 8. compile and link the kernel code:
+
+  const char *options = "";
+  status = clBuildProgram(program, 1, &device, options, NULL, NULL);
+  if (status != CL_SUCCESS) {
+    size_t size;
+    clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, NULL,
+                          &size);
+    cl_char *log = new cl_char[size];
+    clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, size, log,
+                          NULL);
+    fprintf(stderr, "clBuildProgram failed:\n%s\n", log);
+    delete[] log;
+  }
+
+  // 9. create the kernel object:
+
+  cl_kernel kernel = clCreateKernel(program, "MultReduce", &status);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "clCreateKernel failed\n");
+
+  // 10. setup the arguments to the kernel object:
+
+  status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &device_A);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "clSetKernelArg failed (1)\n");
+
+  status = clSetKernelArg(kernel, 1, sizeof(cl_mem), &device_B);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "clSetKernelArg failed (2)\n");
+
+  // local array for intermediate sums
+  status = clSetKernelArg(kernel, 2, LOCAL_SIZE * sizeof(float), NULL);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "clSetKernelArg failed (3)\n");
+
+  // global smaller array for workgroup products
+  status = clSetKernelArg(kernel, 3, sizeof(cl_mem), &device_sums);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "clSetKernelArg failed (4)\n");
+
+  // 11. enqueue the kernel object for execution:
+
+  size_t globalWorkSize[3] = {NUM_ELEMENTS, 1, 1};
+  size_t localWorkSize[3] = {LOCAL_SIZE, 1, 1};
+
+  Wait(cmdQueue);
+  double time0 = omp_get_wtime();
+
+  time0 = omp_get_wtime();
+
+  status = clEnqueueNDRangeKernel(cmdQueue, kernel, 1, NULL, globalWorkSize,
+                                  localWorkSize, 0, NULL, NULL);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "clEnqueueNDRangeKernel failed: %d\n", status);
+
+  Wait(cmdQueue);
+  double time1 = omp_get_wtime();
+
+  // 12. read the results buffer back from the device to the host:
+
+  status = clEnqueueReadBuffer(cmdQueue, device_sums, CL_TRUE, 0, sumsSize,
+                               host_sums, 0, NULL, NULL);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "clEnqueueReadBuffer failed\n");
+
+  float sum = 0.;
+  for (int i = 0; i < NUM_WORK_GROUPS; i++) {
+    sum += host_sums[i];
+  }
+
+#ifndef CSV
+  fprintf(stderr, "%8d\t%4d\t%10d\t%10.3lf GigaMultsPerSecond\t%.0f sum\n",
+          NUM_ELEMENTS, LOCAL_SIZE, NUM_WORK_GROUPS,
+          (double)NUM_ELEMENTS / (time1 - time0) / 1000000000., sum);
+#else
+  printf("%d,%d,%d,%lf,%.0f\n", NUM_ELEMENTS, LOCAL_SIZE, NUM_WORK_GROUPS,
+         (double)NUM_ELEMENTS / (time1 - time0) / 1000000000., sum);
+#endif
+
+  // 13. clean everything up:
+
+  clReleaseKernel(kernel);
+  clReleaseProgram(program);
+  clReleaseCommandQueue(cmdQueue);
+  clReleaseMemObject(device_A);
+  clReleaseMemObject(device_B);
+  clReleaseMemObject(device_sums);
+
+  delete[] host_A;
+  delete[] host_B;
+  delete[] host_sums;
+
+  return 0;
+}
+
+int LookAtTheBits(float fp) {
+  int *ip = (int *)&fp;
+  return *ip;
+}
+
+// wait until all queued tasks have taken place:
+
+void Wait(cl_command_queue queue) {
+  cl_event wait;
+  cl_int status;
+
+  status = clEnqueueMarker(queue, &wait);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "Wait: clEnqueueMarker failed\n");
+
+  status = clWaitForEvents(1, &wait);
+  if (status != CL_SUCCESS)
+    fprintf(stderr, "Wait: clWaitForEvents failed\n");
+}
--- cs475-parallel/project-6/report.md
+++ cs475-parallel/project-6/report.md
@@ -8,33 +8,26 @@ header-includes:
   - \def\fps@figure{H}
   - \makeatother
 
-title: 'CS 475 Project 5: CUDA Monte Carlo Simulation'
+title: 'CS 475 Project 6: OpenCL Array Operations'
 author:
   - Robert Detjens
   - detjensr@oregonstate.edu
+
+colorlinks: true
+linkcolor: blue
 ---
 
-## Runtime information
+# Runtime information
 
 These benchmarks were done on my local machine, with an AMD Ryzen 2600 CPU and RX480 GPU.
 
 The RX480 only supports work group sizes of up to 256, so these tests go from 8 to 256 instead of up to 512.
 
+# Array Mult/MultSum
+
 ## Data
 
-```table
----
-include: results-mult.csv
-caption: OpenCL Multiply Data
----
-```
-
-```table
----
-include: results-multadd.csv
-caption: OpenCL Multiply & Add Data
----
-```
+Tables are at the end of the report in [Appendix: Tables] since they are quite long.
 
 \newcommand{\globalsize}{1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576,2097152,4194304,8388608}
 \newcommand{\globallabel}{1K  ,    ,    ,8K  ,     ,     ,64K  ,      ,      ,      ,1M     ,       ,       ,8M    }
@@ -57,19 +50,20 @@ caption: OpenCL Multiply & Add Data
   \begin{tikzpicture}
     \begin{axis}[
       xmode = log,
-      ymode = log,
       xlabel = {Global Array Size},
-      ylabel = {Performance (GMult/s) (log scale)},
+      ylabel = {Performance (GMult/s)},
       xtick/.expand once = \globalsize,
       xticklabels/.expand once = \globallabel,
     ]
+      \addlegendimage{empty legend}
+      \addlegendentry{Local workgroup size}
       \foreach \L in \localsize {
         \addplot table[col sep=comma,x=globalsize,y=\L]{results-mult-global.csv};
-        \addlegendentryexpanded{\L \ local workgroup size}
+        \addlegendentryexpanded{\L}
       }
      \end{axis}
   \end{tikzpicture}
-  \caption{Performance vs. Global Array Size across different local sizes}
+  \caption{ArrayMult Performance vs. Global Array Size across different local sizes}
 \end{figure}
 
 \begin{figure}[H]
@@ -81,13 +75,113 @@ caption: OpenCL Multiply & Add Data
       ylabel = {Performance (GMult/s)},
       xtick/.expand once = \localsize,
     ]
+      \addlegendimage{empty legend}
+      \addlegendentry{Global array size}
       \foreach \G in \globalsize {
         \addplot table[col sep=comma,x=localsize,y=\G]{results-mult-local.csv};
-        \addlegendentryexpanded{\G \ global array size}
+        \addlegendentryexpanded{\G}
       }
      \end{axis}
   \end{tikzpicture}
-  \caption{Performance vs. Local Work Size across different global sizes}
+  \caption{ArrayMult Performance vs. Local Work Size across different global sizes}
 \end{figure}
 
-## Analysis
+\begin{figure}[H]
+  \centering
+  \begin{tikzpicture}
+    \begin{axis}[
+      xmode = log,
+      xlabel = {Global Array Size},
+      ylabel = {Performance (GMult/s)},
+      xtick/.expand once = \globalsize,
+      xticklabels/.expand once = \globallabel,
+    ]
+      \addlegendimage{empty legend}
+      \addlegendentry{Local workgroup size}
+      \foreach \L in \localsize {
+        \addplot table[col sep=comma,x=globalsize,y=\L]{results-multadd-global.csv};
+        \addlegendentryexpanded{\L}
+      }
+     \end{axis}
+  \end{tikzpicture}
+  \caption{ArrayMultAdd Performance vs. Global Array Size across different local sizes}
+\end{figure}
+
+\begin{figure}[H]
+  \centering
+  \begin{tikzpicture}
+    \begin{axis}[
+      xmode = log,
+      xlabel = {Local Workgroup Size},
+      ylabel = {Performance (GMult/s)},
+      xtick/.expand once = \localsize,
+    ]
+      \addlegendimage{empty legend}
+      \addlegendentry{Global array size}
+      \foreach \G in \globalsize {
+        \addplot table[col sep=comma,x=localsize,y=\G]{results-multadd-local.csv};
+        \addlegendentryexpanded{\G}
+      }
+     \end{axis}
+  \end{tikzpicture}
+  \caption{ArrayMultAdd Performance vs. Local Work Size across different global sizes}
+\end{figure}
+
+# Reduction
+
+\begin{figure}[H]
+  \centering
+  \begin{tikzpicture}
+    \begin{axis}[
+      xmode = log,
+      xlabel = {Global Array Size},
+      ylabel = {Performance (GMult/s)},
+      xtick/.expand once = \globalsize,
+      xticklabels/.expand once = \globallabel,
+    ]
+      \addlegendimage{empty legend}
+      \addlegendentry{Local workgroup size}
+      \foreach \L in \localsize {
+        \addplot table[col sep=comma,x=globalsize,y=\L]{results-multadd-global.csv};
+        \addlegendentryexpanded{\L}
+      }
+     \end{axis}
+  \end{tikzpicture}
+  \caption{Reduction Performance vs. Global Array Size across different local sizes}
+\end{figure}
+
+# Analysis
+
+For all of these trials, the larger the global size, the higher the performance. As GPU compute is highly parallel, the GPU is able to process these very large arrays and have more of the silicon doing work. Smaller global sizes have smaller performance as there is less work overall being done, as less of the GPU is doing stuff.
+
+Larger local sizes also beget larger performance (up to a point on my local GPU). A larger local workgroup size can better saturate the cores in a compute unit instead of leaving some idle with smaller sizes.
+
+My local GPU (AMD RX 480) has 36 compute units and a max workgroup size of 256, but performance gains level off at a local size of 64.
+
+## Multiply vs Multiply & Add
+
+The addition of addition to the OCL job brought performance down by about 14%. This would be due to the extra time needed to complete the addition operation; although there is a fused-multiply-add operation in hardware that can do part of the addition while the multiply is finishing, the extra time to add the last part together does still take longer.
+
+
+# Appendix: Tables
+
+```table
+---
+include: results-mult.csv
+caption: OpenCL Multiply Data
+---
+```
+
+```table
+---
+include: results-multadd.csv
+caption: OpenCL Multiply & Add Data
+---
+```
+
+```table
+---
+include: results-reduce.csv
+caption: OpenCL Reduction Data
+---
+```
